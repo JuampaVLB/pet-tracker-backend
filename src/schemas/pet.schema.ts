55dcafa280144c23221ceadcd04889c46a9058cc
JuampaVLB <juampablovillalba25@gmail.com>
2023-06-20 20:22:52 -0300

    detail fix on petSchema v2

--- src/schemas/pet.schema.ts
+++ src/schemas/pet.schema.ts
@@ -12,7 +12,7 @@ export const petSchema = z.object({
             .nonempty('La raza es requerida.')
             .min(4, "Se necesita un minimo de 4 caracteres para la raza."),
         size: z.enum(['small', 'medium', 'big', 'verybig'], {
-            required_error: 'El tamaño es requerido',
+            errorMap: () => ({ message: 'El tamaño es requerido' })
           }),
         genre: z.enum(['male', 'female']),
         collar: z.boolean()
